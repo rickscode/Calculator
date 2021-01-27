f53818fb5a582976834b9ffb52e25b7ffadc7f04
“Ricky <grickyh@gmail.com>
2021-01-27 17:51:20 +0700

    Calculator functions

---
+++ script.js
@@ -0,0 +1,23 @@
+// Function for adding
+
+const add = (a, b) => {return a + b};
+
+// Function for subtracting
+
+const subtract = (a, b) => {return a - b};
+
+// Function for multiplying 
+
+const multiply = (a, b) => {return a * b};
+
+// Function for dividing 
+
+const divide = (a, b) => {return a / b};
+
+// Function for sum array
+
+const sum = function (array) {
+  return array.reduce((sum, current) => sum + current, 0);
+};
+
+

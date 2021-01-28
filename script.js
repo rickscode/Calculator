468f2d9c88dfdd1fff87978b43e99ad39256629d
“Ricky <grickyh@gmail.com>
2021-01-28 17:41:59 +0700

    Added function to display numbers pressed on the screen

--- script.js
+++ script.js
@@ -14,15 +14,36 @@ const multiply = (a, b) => {return a * b};
 
 const divide = (a, b) => {return a / b};
 
+// Variable for number input
+
+let displayValue = "";
+
 // Function for sum array
 
 // const sum = function (array) {
 //   return array.reduce((sum, current) => sum + current, 0);
 // };
 
+let buttons = document.querySelectorAll(".number");
+
+buttons.forEach((button) => {
+
+// Click event listener wich calls gameRound function 
+button.addEventListener('click', screen);       
+});
+
+// Function to display numbers pressed on display 
+function screen(e) {
+    displayValue += e.target.textContent;
+    console.log(displayValue);
+    document.getElementById("display").textContent = displayValue;
+}
+
+
 // Create a new function operate that 
 // takes an operator and 2 numbers and then calls 
 // one of the above functions on the numbers.
+
 
 const operate = function (operator, a, b) {
     switch(operator) {

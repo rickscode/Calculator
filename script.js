b606beaa57334329a37682296615f50ad9b4c5fa
“Ricky <grickyh@gmail.com>
2021-01-28 16:14:23 +0700

    Updated styling and code

--- script.js
+++ script.js
@@ -16,8 +16,29 @@ const divide = (a, b) => {return a / b};
 
 // Function for sum array
 
-const sum = function (array) {
-  return array.reduce((sum, current) => sum + current, 0);
-};
+// const sum = function (array) {
+//   return array.reduce((sum, current) => sum + current, 0);
+// };
+
+// Create a new function operate that 
+// takes an operator and 2 numbers and then calls 
+// one of the above functions on the numbers.
+
+const operate = function (operator, a, b) {
+    switch(operator) {
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return subtract(a,b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a,b);
+    }
+
+}
 
 

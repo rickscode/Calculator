023a01f5abeb24ae5a747cd85ccb9fe37f910f05
“Ricky <grickyh@gmail.com>
2021-01-30 02:39:32 +0700

    Added functions for storing all arguments, operators and a function for calculating those bindings

--- script.js
+++ script.js
@@ -14,38 +14,97 @@ const multiply = (a, b) => {return a * b};
 
 const divide = (a, b) => {return a / b};
 
-// Variable for number input
+// Binding for display value
 
 let displayValue = "";
 
-// Function for sum array
+// Binding for operator value 
 
-// const sum = function (array) {
-//   return array.reduce((sum, current) => sum + current, 0);
-// };
+let operatorValue = null;
 
+
+let numInput = "";
+
+// Binding for firstNum
+
+let firstNum = "";
+
+// Binding for secondNum
+
+let secondNum = "";
+
+// Binding for DOM selection .number
 let buttons = document.querySelectorAll(".number");
 
+// Create a node list 
 buttons.forEach((button) => {
 
-// Click event listener wich calls gameRound function 
+// Click event listener wich calls screen function 
 button.addEventListener('click', screen);       
 });
 
 // Function to display numbers pressed on display 
+
 function screen(e) {
     displayValue += e.target.textContent;
-    console.log(displayValue);
+    console.log("display", displayValue);
     document.getElementById("display").textContent = displayValue;
 }
 
+// Step 5
 
-// Create a new function operate that 
-// takes an operator and 2 numbers and then calls 
-// one of the above functions on the numbers.
+// Binding for DOM selection .operator
+
+let pressOperator = document.querySelectorAll(".operator")
+
+// Create a node list 
+
+pressOperator.forEach((button) => {
+
+// Click event listener wich calls storeInput function 
+button.addEventListener('click', storeInput);       
+});
+
+// git push to here
+
+function storeInput(e) {
+    
+    if(operatorValue !== null) check();
+    firstNum = parseInt(displayValue);
+    console.log("first", firstNum);
+    
+    operatorValue = e.target.textContent;
+    console.log("operator", operatorValue);
+    
+    // clear display
+    displayValue = "";
+    // document.getElementById("display").textContent = operatorValue;
+    // console.log("display", displayValue);
+}
+
+// git push to here 
+
+function check() {
+    secondNum = parseInt(displayValue);
+    console.log("second", secondNum);
+    document.getElementById("display").textContent = operate(operatorValue, firstNum,secondNum);
+    
+    displayValue = document.getElementById("display").textContent;
+    console.log("display", displayValue);
+}
 
 
-const operate = function (operator, a, b) {
+
+let equate = document.querySelector(".equals");
+
+equate.addEventListener('click', answer);
+
+function answer(e) {
+    check();
+    }
+ 
+
+    const operate = function (operator, a, b) {
     switch(operator) {
         case "+":
             return add(a,b);
@@ -53,13 +112,13 @@ const operate = function (operator, a, b) {
         case "-":
             return subtract(a,b);
             break;
-        case "*":
+        case "x":
             return multiply(a, b);
             break;
         case "/":
             return divide(a,b);
     }
-
 }
+
 
 
